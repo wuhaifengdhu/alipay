995976589e7d4b59df72071dfae2ba6778a9a6e9
wuhaifengdhu <wuhaifengdhu@163.com>
2018-01-06 00:20:49 +0800

    add more

--- index.js
+++ index.js
@@ -5,9 +5,15 @@ var cookieParser        = require('cookie-parser') ;
 app.use(cookieParser()) ;
 app.use(bodyParse.urlencoded({extended:false})) ;
 
-app.get('/',function(req,res){
+app.get('/me',function(req,res){
     res.sendfile('home.html') ;
     console.log('main page is required ');
-}) ;
+});
 
-var server=app.listen(3000) ;
+
+app.get('/gan',function(req,res){
+    res.sendfile('agan.html') ;
+    console.log('main page is required ');
+});
+
+var server=app.listen(80) ;
