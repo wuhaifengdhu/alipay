f4aa26b727cf790214b64ee0150cb80fa21cd6cf
wuhaifengdhu <wuhaifengdhu@163.com>
2018-01-06 00:34:07 +0800

    fixed one bug

--- index.js
+++ index.js
@@ -5,15 +5,16 @@ var cookieParser        = require('cookie-parser') ;
 app.use(cookieParser()) ;
 app.use(bodyParse.urlencoded({extended:false})) ;
 
+app.get('/gan',function(req,res){
+    res.sendfile('gan.html') ;
+    console.log('gan page is required ');
+});
+
 app.get('/me',function(req,res){
     res.sendfile('home.html') ;
     console.log('main page is required ');
 });
 
 
-app.get('/gan',function(req,res){
-    res.sendfile('agan.html') ;
-    console.log('main page is required ');
-});
 
 var server=app.listen(80) ;
