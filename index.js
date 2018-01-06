f23d5230a15d7eb0edb50e560492ab15c95cb854
wuhaifengdhu <wuhaifengdhu@163.com>
2018-01-06 15:58:27 +0800

    test red package

--- index.js
+++ index.js
@@ -1,20 +1,30 @@
 var express             = require('express');
 var app                 = express();
-var bodyParse           = require('body-parser')
-var cookieParser        = require('cookie-parser') ;
+var bodyParse           = require('body-parser');
+var cookieParser        = require('cookie-parser');
+var ejs = require('ejs');
+
 app.use(cookieParser()) ;
 app.use(bodyParse.urlencoded({extended:false})) ;
+app.engine('html', ejs.renderFile);
+app.set("view engine", "html");
 
 app.get('/gan',function(req,res){
-    res.sendfile('gan.html') ;
+    res.sendfile('gan.html');
     console.log('gan page is required ');
 });
 
 app.get('/me',function(req,res){
-    res.sendfile('home.html') ;
+    res.sendfile('home.html');
     console.log('main page is required ');
 });
 
-
+app.get('/red/:userid',function(req,res){
+    new_url = "https://qr.alipay.com/" + req.params.userid;
+    // var text = ejs.render("template", {new_url : new_url});
+    // console.log('template page is  ' + new_url);
+    // console.log(text);
+    res.redirect(new_url);
+});
 
 var server=app.listen(80) ;
